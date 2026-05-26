d87310b0e8b0e8a0454a9a87537e9ebbe01369d2
vhzzlk <vhzzlk@gmail.com>
2026-05-26 11:00:55 -0300

    feat: add all

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import styled from 'styled-components/native';
 import { Home } from './src/pages/home';
-import { SafeAreaView } from 'react-native';
-
-
-const AppView = styled.SafeAreaView`
-flex: 1;`
-const Status = styled.StatusBar``
-const Title = styled.Text`
-font-size: 150;
-color:blue;
-`
+import { SafeAreaView, ActivityIndicator, View } from 'react-native';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins: Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#0D1B34" />
+      </View>
+    );
+  }
+
   return (
-    <AppView>
-      <Status />
-      <Home/>
-    </AppView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar hidden />
+      <Home />
+    </SafeAreaView>
   );
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,22 +1,36 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
 export const Container = styled.View`
-flex: 1;
-border: 4px solid red;
-background-color: blue;
-`
+  flex: 1;
+  border: 4px solid red;
+  display: flex;
+  padding: 64px 24px;
+  background-color: #fff;
+`;
 export const Greetings = styled.View`
-
-`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid black;
+`;
 export const GreetingsTitle = styled.View`
-
-`
+  display: flex;
+  gap: 6px;
+`;
 export const GreetingsSpan = styled.Text`
-
-`
+  font-family: 'Poppins';
+  color: #8696bb;
+  font-size: 16px;
+`;
 export const GreetingsText = styled.Text`
-
-`
+  font-family: 'Poppins_700Bold';
+  color: #0d1b34;
+  font-size: 20px;
+  line-height: 22px;
+`;
 export const GreetingsAvatar = styled.Image`
-
-`
+  width: 56px;
+  height: 56px;
+`;
